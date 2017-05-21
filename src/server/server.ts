42a72d533447749547e1b92cd17363ed8320736f
Devin Truong <truongdevin@yahoo.com>
2017-05-20 22:03:38 -0700

    converted server files to typescript

---
+++ src/server/server.ts
@@ -0,0 +1,14 @@
+import * as express from "express"
+import * as path from "path"
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../../public')))
+app.use(express.static(path.join(__dirname, '../../dist')))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'))
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000!")
+})
