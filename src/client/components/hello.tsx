58cc70d9d79889de35e4d46c3b39a8f5a9252b16
Devin Truong <truongdevin@yahoo.com>
2017-05-21 20:13:29 -0700

    added ability to draw

--- src/client/components/hello.tsx
+++ src/client/components/hello.tsx
@@ -1,11 +1,61 @@
 import * as React from "react"
 
 export default class Hello extends React.Component<{}, {}> {
+    canvas: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D
+    width: number = 500
+    height: number = 500
+    mouseDown: boolean = false
+
+    componentDidMount = () => {
+        this.canvas = document.querySelector(".whiteboard") as HTMLCanvasElement
+        this.ctx = this.canvas.getContext("2d")
+        this.ctx.lineWidth = 3
+        this.ctx.strokeStyle = "red"
+        this.ctx.lineCap = "round"
+        this.ctx.lineJoin = "round"
+    }
+
+    handleMouseDown = (e) => {
+        this.mouseDown = true
+        const pos = this.getMousePos(e)
+        this.ctx.beginPath()
+        this.ctx.moveTo(pos.x, pos.y)
+    }
+
+    handleMouseMove = (e) => {
+        if (this.mouseDown) {
+            const pos = this.getMousePos(e)
+            this.ctx.lineTo(pos.x, pos.y)
+            this.ctx.stroke()
+        }
+    }
+
+    handleMouseUp = (e) => {
+        this.mouseDown = false
+    }
+
+    getMousePos = (evt) => {
+        const rect = this.canvas.getBoundingClientRect()
+        return {
+            x: Math.floor(evt.clientX - rect.left),
+            y: Math.floor(evt.clientY - rect.top)
+        }
+    }
+
     render() {
         return (
         <div>
-            <h1>Hello from React</h1>
-            <div className="red">I am red.</div>
+            <h1>Draw App</h1>
+            <div className="red">Draw on me!</div>
+            <canvas 
+                className="whiteboard" 
+                width={`${this.width}px`} 
+                height={`${this.height}px`}
+                onMouseDown={this.handleMouseDown}
+                onMouseMove={this.handleMouseMove}
+                onMouseUp={this.handleMouseUp} 
+                />
         </div>
         )
     }
